c88baefe09e8e04e6c8b39b7c775abb2c5758487
Manon Blanke <manon-elisabeth.blanke@ubisoft.com>
2025-08-05 20:03:38 -0400

    Create graphics pipeline and main render pass

--- src/graphics/vertex.h
+++ src/graphics/vertex.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../utils/precomp.h"
+#include <vulkan/vulkan.h>
 
 struct Vertex {
 	Vertex() : position(float3(0.0f)), uv(float2(0.0f)) {}
@@ -8,4 +9,28 @@ struct Vertex {
 
 	float3 position;
 	float2 uv;
+
+	static VkVertexInputBindingDescription GetBindingDescription() {
+		VkVertexInputBindingDescription description = {};
+		description.binding = 0;
+		description.stride = sizeof(Vertex);
+		description.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+		return description;
+	}
+
+	static std::array<VkVertexInputAttributeDescription, 2> GetAttributeDescriptions() {
+		std::array<VkVertexInputAttributeDescription, 2> descriptions = {};
+		descriptions[0].binding = 0;
+		descriptions[0].location = 0;
+		descriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+		descriptions[0].offset = offsetof(Vertex, position);
+
+		descriptions[1].binding = 0;
+		descriptions[1].location = 1;
+		descriptions[1].format = VK_FORMAT_R32G32_SFLOAT;
+		descriptions[1].offset = offsetof(Vertex, uv);
+
+		return descriptions;
+	}
 };
