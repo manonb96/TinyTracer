c1d22351dad2cbab522f455ee613414bba5d21a4
Manon Blanke <manon-elisabeth.blanke@ubisoft.com>
2025-08-05 20:02:05 -0400

    Add ReadFile-method

--- src/utils/utilities.cpp
+++ src/utils/utilities.cpp
@@ -1,7 +1,32 @@
 #include "precomp.h"
 #include "utilities.h"
+#include <fstream>
+#include <spdlog/spdlog.h>
 
 bool streq(gsl::czstring left, gsl::czstring right) {
 	return std::strcmp(left, right) == 0;
+}
+
+std::vector<std::uint8_t> ReadFile(std::filesystem::path file_path) {
+	if (!std::filesystem::exists(file_path)) {
+		spdlog::error("No file found at {}", file_path.string());
+		return {};
+	}
+
+	if (!std::filesystem::is_regular_file(file_path)) {
+		spdlog::error("File at {} is not a regular file", file_path.string());
+		return {};
+	}
+
+	std::ifstream file(file_path, std::ios::binary);
+	if (!file.is_open()) {
+		spdlog::error("Could not open file at {}", file_path.string());
+		return {};
+	}
+
+	const std::uint32_t size = std::filesystem::file_size(file_path);
+	std::vector<std::uint8_t> buffer(size);
+	file.read(reinterpret_cast<char*>(buffer.data()), size);
+	return buffer;
 }
 
