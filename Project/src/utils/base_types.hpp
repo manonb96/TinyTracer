e05c5fa0c200d1a1f1d6b82d1cdfe2675522210b
manonb96 <manonblanke_@hotmail.com>
2025-09-20 17:45:59 -0400

    Refactor base types

--- Project/src/utils/base_types.hpp
+++ Project/src/utils/base_types.hpp
@@ -21,35 +21,27 @@ using vector = std::vector<T>;
 // 2D
 struct int2 {
 	int x, y;
-	int2();
-	int2(int a, int b);
+	int2() : x(0), y(0) {}
+	int2(int a, int b) : x(a), y(b) {}
 };
-inline int2::int2()	: x(0), y(0) {}
-inline int2::int2(int a, int b) : x(a), y(b) {}
 inline int2 operator+(int2 a, int2 b) { return int2(a.x + b.x, a.y + b.y); }
 inline int2 operator-(int2 a, int2 b) { return int2(a.x - b.x, a.y - b.y); }
 inline int2 operator/(int2 a, int b) { return int2(a.x / b, a.y / b); }
 
 struct float2 {
 	float x, y;
-	float2();
-	float2(float a);
-	float2(float a, float b);
+	float2() : x(0.0f), y(0.0f) {}
+	float2(float a) : x(a), y(a) {}
+	float2(float a, float b) : x(a), y(b) {}
 };
-inline float2::float2() : x(0.0f), y(0.0f) {}
-inline float2::float2(float a) : x(a), y(a) {}
-inline float2::float2(float a, float b) : x(a), y(b) {}
 
 // 3D
 struct float3 {
 	float x, y, z;
-	float3();
-	float3(float a);
-	float3(float a, float b, float c);
+	float3() : x(0.0f), y(0.0f), z(0.0f) {}
+	float3(float a) : x(a), y(a), z(a) {}
+	float3(float a, float b, float c) : x(a), y(b), z(c) {}
 };
-inline float3::float3() : x(0.0f), y(0.0f), z(0.0f) {}
-inline float3::float3(float a) : x(a), y(a), z(a) {}
-inline float3::float3(float a, float b, float c) : x(a), y(b), z(c) {}
 inline float rsqrtf(float x) { return 1.0f / sqrtf(x); }
 inline float3 operator+(float3 a, float3 b) { return float3(a.x + b.x, a.y + b.y, a.z + b.z); }
 inline float3 operator-(float3 a, float3 b) { return float3(a.x - b.x, a.y - b.y, a.z - b.z); }
